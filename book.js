cf24a29a843d8c798c462b2294e18fc38eef7f4d
steven <steven@burrows.plus.com>
2020-05-16 16:04:19 +0100

    changed to classes

---
+++ book.js
@@ -0,0 +1,20 @@
+ class Book{
+  constructor(author, title, genre) {
+  this.author = author;
+  this.title = title;
+  this.genre = genre;
+  }
+
+  
+}
+
+export default Book;
+
+
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+// module.exports = Book;
